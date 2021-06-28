07de97c3da6e1e1537c4a830c7938f29c248754d
Hatvis7 <shamil.gasanov.2004@bk.ru>
2021-06-28 17:31:05 +0300

    Закончена работа над адаптивностью шапки

---
+++ dist/source/js/script.js
@@ -0,0 +1,8 @@
+(function() {
+    
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger__active');
+    });
+    
+}());
