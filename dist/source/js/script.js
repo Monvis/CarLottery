405a79ceb190de93220d93e2f606c29649516925
Hatvis7 <shamil.gasanov.2004@bk.ru>
2021-07-06 00:24:20 +0300

    Создан swiperJs блок с незаконченным адаптивом

--- dist/source/js/script.js
+++ dist/source/js/script.js
@@ -1,8 +1,25 @@
-(function() {
-    
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('burger__active');
-    });
-    
-}());
+//burgerMenu
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger__active');
+});
+
+//swiperJs
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+/// Learning
+
+firstFor: for (let num = 0; num < 2; num++) {
+    for (let size = 0; size < 3; size++) {
+        if (size == 1) break firstFor;
+        console.log(size);
+    }
+}
